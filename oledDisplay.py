96812b2a5312444a68ea2bac078c5152869c4fa2
MechkarovTUES <aleksandar.i.mechkarov.2020@elsys-bg.org>
2024-03-24 16:07:58 +0000

    image display script

---
+++ oledDisplay.py
@@ -0,0 +1,25 @@
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+
+# Change these
+# to the right size for your display!
+WIDTH = 128
+HEIGHT = 64  # Change to 64 if needed
+BORDER = 5
+
+# Use for I2C.
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
+oled.fill(0)
+oled.show()
+image = Image.open('Mladshi_128x64.ppm').convert('1')
+# Display image
+oled.image(image)
+oled.show()
