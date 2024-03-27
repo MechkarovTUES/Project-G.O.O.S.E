f9e1c1960042422f9a225b80a1a3db253262941e
MechkarovTUES <aleksandar.i.mechkarov.2020@elsys-bg.org>
2024-03-27 16:09:57 +0000

    implemented dynamic scrolling

--- oledDisplay.py
+++ oledDisplay.py
@@ -22,8 +22,9 @@ class Menu:
         self.options = []
         self.image = Image.new('1', (oled.width, oled.height))
         self.draw = ImageDraw.Draw(self.image)
-        self.font = ImageFont.load_default()  # List to hold menu options
-        self.index = 0  # Current index of the displayed options
+        self.font = ImageFont.load_default()
+        self.index = 0  # Current selection index
+        self.start_index = 0  # Start index of the visible options
 
     def add_option(self, option):
         self.options.append(option)
@@ -35,22 +36,31 @@ class Menu:
 
     def display_menu(self):
         self.clear()
-        
-        start_index = self.index
-        end_index = min(start_index + 3, len(self.options))
-        print(start_index, end_index, self.index) #check the index status
+        visible_count = 3  # Number of options visible at once
+        end_index = min(self.start_index + visible_count, len(self.options))
 
-        self.draw.rectangle((0, 0, self.oled.width - 1, 20), outline=255, fill=0) # draw rectangle around selected option
-        
-        for i in range(start_index, end_index):
-            y = (i - start_index) * 20 + 5
+        for i in range(self.start_index, end_index):
+            y = (i - self.start_index) * 20 + 5
+            if i == self.index:
+                self.draw.rectangle((0, y - 5, self.oled.width - 1, y + 15), outline=255, fill=0)  # Highlight selected option
             self.draw.text((5, y), self.options[i], font=self.font, fill=255)
-        
-        
-        
 
         self.oled.image(self.image)
         self.oled.show()
+
+    def scroll_down(self):
+        if self.index < len(self.options) - 1:
+            self.index += 1
+            if self.index >= self.start_index + 3:  # If the selection moves beyond the visible options
+                self.start_index += 1  # Scroll options down
+
+    def scroll_up(self):
+        if self.index > 0:
+            self.index -= 1
+            if self.index < self.start_index:  # If the selection moves before the visible options
+                self.start_index -= 1  # Scroll options up
+    def calcSize(self):
+        print(self.font.getsize(self.options[2])[0])
 
 # create a Menu object
 menu = Menu(oled)
@@ -58,7 +68,7 @@ menu = Menu(oled)
 # add some options to the menu
 menu.add_option("Config")
 menu.add_option("NFC Module")
-menu.add_option("Bluetooth")
+menu.add_option("Pair with Bluetooth")
 menu.add_option("Option 4")
 menu.add_option("Option 5")
 menu.add_option("Option 6")
@@ -70,7 +80,15 @@ menu.display_menu()
 
 # test scrolling
 time.sleep(1)
-for index in range(len(menu.options)-1):
-    menu.index += 1
-    menu.display_menu()
-    time.sleep(1)
+# for i in range(2):
+#     menu.scroll_down()
+#     menu.display_menu()
+#     time.sleep(1)
+
+menu.calcSize()
+
+# time.sleep(1)
+# for i in range(5):
+#     menu.scroll_up()
+#     menu.display_menu()
+#     time.sleep(1)
